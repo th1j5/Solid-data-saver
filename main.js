cd8863956d4c39f9480d4c891d36d83635e4f3fa
FlorSanders <florsanders@gmail.com>
2020-04-11 17:55:34 +0200

    Made some changes, still not working

--- main.js
+++ main.js
@@ -8,7 +8,6 @@
 // Importing required libraries
 const fs = require('fs');    // File system to read in the static file
 const auth = require('solid-auth-cli');  // Solid authorization library for node/command line
-const cli = require('solid-cli')
 
 // Program parameters
 const filename = "static.ttl";  // Turtle file with static sensor data
